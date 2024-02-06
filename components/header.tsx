612ba9516c34db3392974815b0285908580859f9
callumhayden <callum.hayden@gmail.com>
2024-02-06 18:07:25 +0000

    header foundation started

---
+++ components/header.tsx
@@ -0,0 +1,16 @@
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <>
+      <div className="header-container">
+        <div>Home</div>
+        <div>About</div>
+        <div>Services</div>
+        <div>Portfolio</div>
+        <div>Prices</div>
+        <div>Contact</div>
+      </div>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import NavMenu from "../components/navmenu";
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 
 import Welcome from "../components/welcome";
+import Header from "../components/header";
 import QandAnswers from "../components/qandanswers";
-
 
 import "@/app/globals.css";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <NavMenu />
-      
       <Welcome />
       <QandAnswers />
     </>
