7410fa9549536200541480a82c895ddb434a6489
callumhayden <callum.hayden@gmail.com>
2024-02-06 18:27:52 +0000

    basic header structure started

--- components/header.tsx
+++ components/header.tsx
@@ -5,11 +5,19 @@ export default function Header() {
     <>
       <div className="header-container">
         <div>Home</div>
-        <div>About</div>
-        <div>Services</div>
+        <div className="about-container">
+          <a href="#about-a">
+            <div>About</div>
+          </a>
+        </div>
+        <a href="#services-a">
+          <div>Services</div>
+        </a>
         <div>Portfolio</div>
         <div>Prices</div>
-        <div>Contact</div>
+        <a href="#contact-a">
+          <div>Contact</div>
+        </a>
       </div>
     </>
   );
